c6416c37e7a0bf152b20feb7ddb4a66d1a989cf8
Deathmax <death@deathmax.tk>
2014-02-08 10:49:37 +0800

    catch exceptions when trying to get ringtone name

--- WarDroid/src/main/java/com/deathsnacks/wardroid/activities/NotificationsActivity.java
+++ WarDroid/src/main/java/com/deathsnacks/wardroid/activities/NotificationsActivity.java
@@ -66,13 +66,17 @@ public class NotificationsActivity extends SherlockPreferenceActivity {
             }
         Preference uriPref = findPreference("sound");
         if (uriPref != null) {
-            Uri ringtoneUri = Uri.parse(mPreferences.getString("sound", RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION).toString()));
-            Ringtone ringtone = RingtoneManager.getRingtone(getApplicationContext(), ringtoneUri);
-            String name = ringtone.getTitle(getApplicationContext());
-            if (mPreferences.getString("sound", "") == "") {
-                uriPref.setSummary("None");
-            } else {
-                uriPref.setSummary(name);
+            try {
+                Uri ringtoneUri = Uri.parse(mPreferences.getString("sound", RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION).toString()));
+                Ringtone ringtone = RingtoneManager.getRingtone(getApplicationContext(), ringtoneUri);
+                String name = ringtone.getTitle(getApplicationContext());
+                if (mPreferences.getString("sound", "") == "") {
+                    uriPref.setSummary("None");
+                } else {
+                    uriPref.setSummary(name);
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
             }
         }
         //}
